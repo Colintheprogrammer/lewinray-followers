32bf3410e118d6f6a29ccdeb0d50d0b1648b0d90
Colintheprogrammer <colin.schuch@gmail.com>
2025-06-04 21:53:14 +0200

    Update update_followers.py

--- update_followers.py
+++ update_followers.py
@@ -3,13 +3,11 @@ import requests
 from datetime import datetime
 
 RAPIDAPI_KEY = os.environ["RAPIDAPI_KEY"]
-
-# TikTok
 TIKTOK_USER_ID = "56605052018139136"
+INSTAGRAM_USERNAME = "lewinray"
 
-# Instagram
-INSTAGRAM_PROFILE_URL = "https://www.instagram.com/lewinray/"
-
+def format_number_spaced(number):
+    return f"{number:,}".replace(",", " ")
 
 def get_tiktok_followers_by_id(user_id):
     url = "https://tiktok-scraper7.p.rapidapi.com/user/info"
@@ -23,10 +21,9 @@ def get_tiktok_followers_by_id(user_id):
     data = response.json()
     return format_number_spaced(data["data"]["stats"]["followerCount"])
 
-
-def get_instagram_followers_by_url(profile_url):
+def get_instagram_followers_by_username(username):
     url = "https://instagram-statistics-api.p.rapidapi.com/profile"
-    querystring = {"url": profile_url}
+    querystring = {"url": f"https://www.instagram.com/{username}/"}
     headers = {
         "X-RapidAPI-Key": RAPIDAPI_KEY,
         "X-RapidAPI-Host": "instagram-statistics-api.p.rapidapi.com"
@@ -36,18 +33,13 @@ def get_instagram_followers_by_url(profile_url):
     data = response.json()
     return format_number_spaced(data["data"]["usersCount"])
 
-
-def format_number_spaced(number):
-    return f"{number:,}".replace(",", " ")
-
-
-def write_html_file(content, filename="followers.html", platform="Followers"):
-    with open(filename, "w", encoding="utf-8") as f:
+def write_html_file(tiktok_count, insta_count):
+    with open("followers.html", "w", encoding="utf-8") as f:
         f.write(f"""<!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
-  <title>{platform}</title>
+  <title>Followers</title>
   <style>
     @import url('https://fonts.googleapis.com/css2?family=Inter:wght@600&display=swap');
     body {{
@@ -57,6 +49,7 @@ def write_html_file(content, filename="followers.html", platform="Followers"):
       align-items: center;
       justify-content: center;
       height: 100vh;
+      gap: 80px;
     }}
     .followers {{
       font-family: 'Inter', sans-serif;
@@ -67,16 +60,12 @@ def write_html_file(content, filename="followers.html", platform="Followers"):
   </style>
 </head>
 <body>
-  <div class="followers">{content}</div>
+  <div class="followers">{tiktok_count}</div>
+  <div class="followers">{insta_count}</div>
 </body>
 </html>""")
 
-
 if __name__ == "__main__":
-    # TikTok
     tiktok_followers = get_tiktok_followers_by_id(TIKTOK_USER_ID)
-    write_html_file(tiktok_followers, filename="tiktok.html", platform="TikTok Followers")
-
-    # Instagram
-    insta_followers = get_instagram_followers_by_url(INSTAGRAM_PROFILE_URL)
-    write_html_file(insta_followers, filename="instagram.html", platform="Instagram Followers")
+    instagram_followers = get_instagram_followers_by_username(INSTAGRAM_USERNAME)
+    write_html_file(tiktok_followers, instagram_followers)
